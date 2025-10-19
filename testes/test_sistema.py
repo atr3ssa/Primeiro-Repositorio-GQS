971add60f99507ec5eb4fbd7609558c2f3efb385
Andressa Rabêlo <dre.emy89@gmail.com>
2025-10-19 14:44:24 -0300

    Update test_sistema.py

--- testes/test_sistema.py
+++ testes/test_sistema.py
@@ -1,7 +1,7 @@
 import unittest # módulo pararealização de testes no Python
 #from sistema_legado import adicionar_usuario, remover_usuario, buscar_usuario, listar_usuarios
 
-from sistema_legado import (
+from sistema.sistema_legado import (
     adicionar_usuario,
     remover_usuario,
     buscar_usuario,
