31b8a53ee3218bebf3e42a566aaa6b301c60b580
Andressa Rabêlo <dre.emy89@gmail.com>
2025-10-19 14:25:12 -0300

    testando sistema_legado, sem o helpers

--- testes/test_sistema.py
+++ testes/test_sistema.py
@@ -1,7 +1,7 @@
 import unittest # módulo pararealização de testes no Python
 #from sistema_legado import adicionar_usuario, remover_usuario, buscar_usuario, listar_usuarios
 
-from test_helpers import (
+from sistema_legado import (
     adicionar_usuario,
     remover_usuario,
     buscar_usuario,
